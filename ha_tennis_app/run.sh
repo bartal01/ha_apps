477340d4e773bb265033b548e8e214f8e6339209
Bartels Alexander <Alexander.Bartels@coc-ag.de>
2026-02-21 17:55:08 +0100

    Initial commit

---
+++ ha_tennis_app/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bashio
+
+# Config auslesen
+REQUIRE_LOGIN=$(bashio::config 'require_login')
+ADMIN_USER=$(bashio::config 'admin_username')
+ADMIN_PASS=$(bashio::config 'admin_password')
+
+# Umgebungsvariablen setzen
+export REQUIRE_LOGIN="$REQUIRE_LOGIN"
+export ADMIN_USERNAME="$ADMIN_USER"
+export ADMIN_PASSWORD="$ADMIN_PASS"
+export DATA_DIR="/config/tennis_stringing"
+export FLASK_APP="app.py"
+
+# Daten-Verzeichnis erstellen
+mkdir -p "$DATA_DIR"
+
+bashio::log.info "Starte Tennis Bespannungs-Manager..."
+bashio::log.info "Daten werden gespeichert in: $DATA_DIR"
+
+# Starte Tennis App
+exec python app.py
